108c39c4c7cf3fd5ca7f4f8fed298057bac22e0a
佐藤 真 <ac.backfill1539@gmail.com>
2022-07-14 22:50:25 +0900

    Initial commit

---
+++ VisionKitTutorial/Sources/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  VisionKitTutorial
+//
+//  Created by 佐藤真 on 2022/06/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+        // Do any additional setup after loading the view.
+    }
+}
