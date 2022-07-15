cb36f72c272da996935e48cdb12862b5eb7e1209
佐藤 真 <ac.backfill1539@gmail.com>
2022-07-16 08:41:01 +0900

    add swiftgen

--- VisionKitTutorial/Sources/ViewController.swift
+++ VisionKitTutorial/Sources/ViewController.swift
@@ -5,12 +5,24 @@
 //  Created by 佐藤真 on 2022/06/15.
 //
 
+import SnapKit
 import UIKit
 
 class ViewController: UIViewController {
+    private let startScanLabel: UILabel = {
+        let label = UILabel()
+        label.text = L10n.startToScan
+        label.layer.borderWidth = 1.0
+        label.layer.borderColor = UIColor.blue.cgColor
+        return label
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
+        view.addSubview(startScanLabel)
+        startScanLabel.snp.makeConstraints { make in
+            make.center.equalToSuperview()
+        }
     }
 }
