f6fbd0e7f138a611c1b0a327307adeb4f3fa2620
佐藤 真 <ac.backfill1539@gmail.com>
2022-07-17 23:19:13 +0900

    add swift-format remove SwiftFormat

--- VisionKitTutorial/Sources/AppDelegate.swift
+++ VisionKitTutorial/Sources/AppDelegate.swift
@@ -11,7 +11,9 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
-    func application(_: UIApplication, didFinishLaunchingWithOptions _: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    func application(
+        _: UIApplication, didFinishLaunchingWithOptions _: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
         // Override point for customization after application launch.
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.rootViewController = ViewController()
@@ -21,7 +23,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     // MARK: UISceneSession Lifecycle
 
-    func application(_: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options _: UIScene.ConnectionOptions) -> UISceneConfiguration {
+    func application(
+        _: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession,
+        options _: UIScene.ConnectionOptions
+    ) -> UISceneConfiguration {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
         UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
@@ -29,7 +34,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_: UIApplication, didDiscardSceneSessions _: Set<UISceneSession>) {
         // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
+        // If any sessions were discarded while the application was not running,
+        // this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
 }
--- VisionKitTutorial/Sources/ViewController.swift
+++ VisionKitTutorial/Sources/ViewController.swift
@@ -7,21 +7,67 @@
 
 import SnapKit
 import UIKit
+import VisionKit
 
 class ViewController: UIViewController {
-    private let startScanLabel: UILabel = {
-        let label = UILabel()
-        label.text = L10n.startToScan
-        label.layer.borderWidth = 1.0
-        label.layer.borderColor = UIColor.blue.cgColor
-        return label
-    }()
+    private let startView = StartView()
+    private var isDeviceCapacity = false
+
+    init() {
+        super.init(nibName: nil, bundle: nil)
+        isDeviceCapacity = DataScannerViewController.isSupported && DataScannerViewController.isAvailable
+    }
+
+    required init?(coder _: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func loadView() {
+        view = startView
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
-        view.addSubview(startScanLabel)
-        startScanLabel.snp.makeConstraints { make in
+        startView.startScanButton.addTarget(self, action: #selector(tappedStartScanButton), for: .touchUpInside)
+    }
+
+    @objc func tappedStartScanButton() {
+        isDeviceCapacity ? presentNextViewController() : presentAlert()
+    }
+
+    private func presentAlert() {
+        let alert = UIAlertController(title: L10n.unavailable, message: nil, preferredStyle: .alert)
+        alert.addAction(.init(title: L10n.yes, style: .default))
+        navigationController?.present(alert, animated: true)
+    }
+
+    private func presentNextViewController() {
+        print("next")
+    }
+}
+
+class StartView: UIView {
+    let startScanButton: UIButton = {
+        let button = UIButton()
+        var config = UIButton.Configuration.filled()
+        config.title = L10n.startToScan
+        button.configuration = config
+        return button
+    }()
+
+    init() {
+        super.init(frame: .zero)
+        setup()
+    }
+
+    required init?(coder _: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    private func setup() {
+        addSubview(startScanButton)
+        startScanButton.snp.makeConstraints { make in
             make.center.equalToSuperview()
         }
     }
